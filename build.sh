fbffd8f0d5ab2d11ee3a1834c5379e4da1f90cf3
huuhungng99 <83088547+huuhungng99@users.noreply.github.com>
2024-12-01 17:50:41 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Cài đặt các gói cần thiết
 pip3 install -r requirements.txt
 
+# Kiểm tra xem lệnh trên có thành công không
+if [ $? -ne 0 ]; then
+    echo "Error installing requirements"
+    exit 1
+fi
+
+# Thu thập các tệp static
 python3 manage.py collectstatic --no-input
+
+# Kiểm tra xem lệnh trên có thành công không
+if [ $? -ne 0 ]; then
+    echo "Error collecting static files"
+    exit 1
+fi
